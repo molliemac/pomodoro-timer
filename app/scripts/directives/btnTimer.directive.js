c051aebd4fd6e9d5992243856ac889a6d3cabaa3
OFFICE\mmcintyre <mollie.mcintyre@elliemae.com>
2018-08-23 13:43:54 -0700

    long break added

--- app/scripts/directives/btnTimer.directive.js
+++ app/scripts/directives/btnTimer.directive.js
@@ -5,8 +5,10 @@
 			templateUrl: '/templates/btnTimer.html',
 			scope: {},
 			link: function(scope, element, attrs) {
-				const WORK_TIME = 10;
-				const BREAK_TIME = 5;
+				var WORK_TIME = 10;
+				var BREAK_TIME = 5;
+				var LONG_BREAK = 10;
+	
 				var countdown;
 
 				scope.currentTime = WORK_TIME;
@@ -14,18 +16,21 @@
 				scope.btnStatus="Start Working Session";
 				scope.onBreak = false;
 				scope.running = false;
+				scope.completed_sessions = 0;
+
 
 				function startTimer() {
 					scope.btnStatus = 'Reset';
 					scope.running = true;
 					countdown = $interval(function() {
-						if (scope.currentTime > 0) {
+						if (scope.currentTime >= 1) {
 							scope.currentTime--;
 						} else {
 							if(scope.onBreak == false) {
 								scope.currentTime = BREAK_TIME;
 								scope.btnStatus = "Give yourself a break!";
 								scope.onBreak = true;
+								scope.completed_sessions++;
 							} else if(scope.onBreak == true) {
 								scope.currentTime = WORK_TIME;
 								scope.btnStatus = "Let's get back to work!";
@@ -38,20 +43,24 @@
 
 
 				function stopTimer() {
-					scope.running = false;
-					if (scope.onBreak == true) {
-						$interval.cancel(countdown);
-						countdown = undefined;
-						scope.btnStatus = "Give yourself a break!";	
-						scope.currentTime = BREAK_TIME;
-					} else if (scope.onBreak == false) {
-						$interval.cancel(countdown);
-						scope.btnStatus = "Start working again!";
+					if (scope.onBreak == false) {
 						scope.currentTime = WORK_TIME;
+						$interval.cancel(countdown);
 						countdown = undefined;
-						
+						scope.btnStatus = "Start Working Again!";
+					} else if (scope.onBreak == true) {
+						if (scope.completed_sessions === 4) {
+							scope.currentTime = LONG_BREAK;
+						} else {
+							scope.currentTime = BREAK_TIME;
+						}
+
+						$interval.cancel(countdown);
+						countdown = undefined;
+						scope.btnStatus = "Give yourself a break!";
 					}
-				};
+					
+				}
 
 				scope.start = function() {
 		          if (countdown) {
